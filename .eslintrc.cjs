ef0f36724f20f069f51465d52289df5d45c6a55c
Juan Torres <torresjjose.03@gmail.com>
2023-03-20 22:15:40 -0300

    Se agregó un método getInstance() estático para emular un singleton más real

---
+++ .eslintrc.cjs
@@ -0,0 +1,22 @@
+module.exports = {
+    
+    "env": {
+        "browser": true,
+        "node": true,
+        "es2021": true
+    },
+    "extends": "eslint:recommended",
+    "overrides": [
+    ],
+    "parserOptions": {
+        "ecmaVersion": "latest",
+        "sourceType": "module"
+    },
+    "rules": {
+        "indent": [
+            "error",
+            4
+        ]
+    }
+}
+
